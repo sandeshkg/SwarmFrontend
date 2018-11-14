4a3f53255759d49992e493bf40d9863134e8ee64
Sandesh <dev.skg@gmail.com>
2018-11-14 14:48:45 +0530

    initial commit

---
+++ FrontEndApp/Controllers/HomeController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FrontEndApp.Models;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace FrontEndApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "More details about your container.";
+            ViewData["IPAddress"] = GetLocalIPAddress();
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            var result = GetAPIResponseAsync();
+            return View();
+        }
+
+        public IActionResult APIResponse()
+        {
+            ViewData["Message"] = "Your API Response page.";
+
+            var result = GetAPIResponseAsync();
+
+            ViewData["Values"] = result.Result;
+
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        public async Task<List<string>> GetAPIResponseAsync()
+        {
+            //Hosted web API REST Service base url  
+            string Baseurl = "http://localhost:50536/";
+
+            List<string> result = new List<string>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("api/Docker/");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    result = JsonConvert.DeserializeObject<List<string>>(EmpResponse);
+
+                }
+                //returning the employee list to view  
+                return result;
+            }
+        }
+    }
+}
